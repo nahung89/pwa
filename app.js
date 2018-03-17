89b185bf931ab5655f7ef93e54309e61b5d8b435
Nah <nahung89@gmail.com>
2018-03-18 00:14:11 +0700

    need to test more

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const newsArticles = document.querySelector('main');
 
-var items;
+var sourceVideos;
 var currentItem;
 
 if ('serviceWorker' in navigator) {
@@ -10,28 +10,31 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.error('SW registration failed: ' + err)));
 }
 
-if ('mediaSession' in navigator) {
-
-  navigator.mediaSession.metadata = new MediaMetadata({
-    title: 'Never Gonna Give You Up',
-    artist: 'Rick Astley',
-    album: 'Whenever You Need Somebody',
-    artwork: [
-      { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-      { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-      { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-      { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-      { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-      { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
-    ]
-  });
-
-  navigator.mediaSession.setActionHandler('play', function() {});
-  navigator.mediaSession.setActionHandler('pause', function() {});
-  navigator.mediaSession.setActionHandler('seekbackward', function() {});
-  navigator.mediaSession.setActionHandler('seekforward', function() {});
-  navigator.mediaSession.setActionHandler('previoustrack', function() {});
-  navigator.mediaSession.setActionHandler('nexttrack', function() {});
+function createMediaSession(source) {
+  if ('mediaSession' in navigator) {
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: source.title,
+      artist: source.creator,
+      album: null,
+      artwork: [
+        { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+        { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+        { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+        { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+        { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+        { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+      ]
+    });
+  
+    navigator.mediaSession.setActionHandler('play', function() {
+      currentItem.play();
+    });
+    navigator.mediaSession.setActionHandler('pause', function() {
+      currentItem.pause();
+    });
+    navigator.mediaSession.setActionHandler('previoustrack', playPrev);
+    navigator.mediaSession.setActionHandler('nexttrack', playNext);
+  }
 }
 
 window.addEventListener('load', e => fetchVibbidi());
@@ -42,22 +45,55 @@ async function fetchVibbidi() {
   newsArticles.innerHTML = '';
   const response = await fetch(`https://api4.vibbidi.com/v4.1/artists/794881599030348/singles?items_to_get=21&start_point=0&mpk=e429d5baeb58a5608e28c5f241e3e500e74f064b2&muid=966313229108987`)
   const json = await response.json();
-  items = json.videos;
+  sourceVideos = json.videos;
   newsArticles.innerHTML = 
      json.videos.map(createVibbidi).join('\n');
 
 }
 
 document.addEventListener("play", function(e) {
-    if(window.$_currentlyPlaying && window.$_currentlyPlaying != e.target) {
-        window.$_currentlyPlaying.pause();
-    } 
-    window.$_currentlyPlaying = e.target;
+  if (currentItem && currentItem != e.target) {
+      currentItem.pause();
+  }
+  currentItem = e.target;
 
-    e.target.addEventListener("ended", function(evt) {
-      playNext(evt.target, evt.target.tagName.toLowerCase());
-    });
+  var items;
+  if (currentItem.tagName.toLowerCase() === 'audio') {
+     items = document.getElementsByTagName('audio');
+  } else {
+     items = document.getElementsByTagName('video');
+  }
+
+  for(var i = 0, len = items.length; i < len;i++){
+    if(items[i] == currentItem) {
+      createMediaSession(sourceVideos[i]);
+    } 
+  };
+
+  e.target.addEventListener("ended", function(evt) {
+    // playNext(evt.target, evt.target.tagName.toLowerCase());
+    playPrev(evt.target, evt.target.tagName.toLowerCase());
+  });
 }, true);
+
+function playPrev(currentItem, type) {
+  var items;
+  if (type === 'audio') {
+     items = document.getElementsByTagName('audio');
+  } else {
+     items = document.getElementsByTagName('video');
+  } 
+
+  for(var i = 0, len = items.length; i < len;i++){
+    if(items[i] == currentItem) {
+      if (i-1 >= 0){
+        items[i-1].play();
+      } else {
+        items[len - 1].play();
+      }
+    } 
+}
+}
 
 function playNext(currentItem, type) {
     var items;
