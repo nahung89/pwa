457c313dc6a2303b8a691ff896a8eb4984cd0afc
Nah <nahung89@gmail.com>
2018-03-19 10:47:13 +0700

    make update cache in service-worker

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var currentItem;
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () =>
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('service-worker.js') // sw.js
       .then(registration => console.log('Service Worker registered'))
       .catch(err => console.error('SW registration failed: ' + err)));
 }
