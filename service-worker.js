86928ef0a74ea3ce276928319472062cf24590e0
Nah <nahung89@gmail.com>
2018-03-20 12:20:01 +0700

    update: remove a litle bit

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var CACHE_VERSION = 7;
+var CACHE_VERSION = 8;
 var CURRENT_CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION,
   dynamic: 'dynamic-cache-v' + CACHE_VERSION
@@ -38,7 +38,6 @@ self.addEventListener('install', function(event) {
   var now = Date.now();
 
   var urlsToPrefetch = [
-  './',
   './index.html',
   './app.js',
   './service-worker.js',
