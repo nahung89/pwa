0ee13699eec8700ac918bb08baa5f017ad57ed3d
Nah <nahung89@gmail.com>
2018-03-22 22:54:43 +0700

    add some sample videos with `cache` in response header

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@
 // cache, then increment the CACHE_VERSION value. It will kick off the service worker update
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
-var CACHE_VERSION = '0.1.21';
+var CACHE_VERSION = '0.1.22';
 var CURRENT_CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION,
   dynamic: 'dynamic-cache-v' + CACHE_VERSION
