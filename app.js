2d4fba437bc3a88e00feeb265bc01011d03bde8e
Nah <nahung89@gmail.com>
2018-03-22 17:05:34 +0700

    try to download and cache api from Angular demo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const newsArticles = document.querySelector('main');
 const statusElement = document.getElementById('status');
-const apiUrl = `https://api4.vibbidi.com/v4.1/artists/794881599030348/singles?items_to_get=5&start_point=0&mpk=e429d5baeb58a5608e28c5f241e3e500e74f064b2&muid=966313229108987`
+const apiUrl = 'https://api4.vibbidi.com/v4.1/artists/794881599030348/singles?items_to_get=5&start_point=0&mpk=e429d5baeb58a5608e28c5f241e3e500e74f064b2&muid=966313229108987'
 
 var sourceVideos;
 var currentItem;
@@ -72,6 +72,15 @@ async function fetchVibbidi() {
   } catch (err) {
     statusElement.innerHTML = "Fail " + err;
   }
+  
+  test();
+}
+
+
+async function test() {
+  const response = await fetch('https://node-hnapi.herokuapp.com/show?page=1');
+  const json = await response.json();
+  console.log('Pre-fetching complete.' + json);
 }
 
 document.addEventListener("play", function(e) {
